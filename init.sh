f48d1e108ed3530d2e2047ed955077acbc6d1d7e
amaya <mail@sapphire.in.net>
2015-06-10 05:22:48 +0900

    powerline!

--- init.sh
+++ init.sh
@@ -8,5 +8,12 @@ ln -s $(pwd)/.zshrc ~/.zshrc
 ln -s $(pwd)/.zsh ~/.zsh
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.vim ~/.vim
+ln -s $(pwd)/.config ~/.config
 ln -s $(pwd)/.gitconfig ~/.gitconfig
 ln -s $(pwd)/.gitignore_global ~/.gitignore_global
+
+./fonts/install.sh
+
+brew install tmux zsh macvim python reattach-to-user-namespace tree
+easy_install pip
+pip install powerline-status psutil
