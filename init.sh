2950cee24883b7eb297665a81eb4c85da2ba17a1
amaya <git@sapphire.in.net>
2016-09-13 00:23:10 +0900

    add os-specific feature

--- init.sh
+++ init.sh
@@ -2,16 +2,16 @@
 git submodule init
 git submodule update
 
-home=$(pwd)
-ln -s $home/.tmux.conf ~/.tmux.conf
-ln -s $home/.tmux ~/.tmux
-ln -s $home/.config ~/.config
-ln -s $home/.zshrc ~/.zshrc
-ln -s $home/.zsh ~/.zsh
-#ln -s $home/.vimrc ~/.vimrc
-#ln -s $home/.vim ~/.vim
-ln -s $home/.gitconfig ~/.gitconfig
-ln -s $home/.gitignore_global ~/.gitignore_global
+dotfiles=$(pwd)
+ln -s $dotfiles/.zsh ~/.zsh
+ln -s $dotfiles/.zshrc ~/.zshrc
+ln -s $dotfiles/.tmux ~/.tmux
+ln -s $dotfiles/.tmux.conf ~/.tmux.conf
+ln -s $dotfiles/.vim ~/.vim
+ln -s $dotfiles/.vimrc ~/.vimrc
+ln -s $dotfiles/.config ~/.config
+ln -s $dotfiles/.gitconfig ~/.gitconfig
+ln -s $dotfiles/.gitignore_global ~/.gitignore_global
 
 if [ `which sudo` ]; then
   prefix='sudo'
@@ -21,15 +21,21 @@ fi
 
 case ${OSTYPE} in
   darwin*)
+    ln -s $dotfiles/osx/.zshrc ~/.zshrc.additional
+    ln -s $dotfiles/osx/.tmux.conf ~/.tmux.conf.additional
     ./fonts/install.sh
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew update && brew upgrade -y && brew install -y tmux zsh python3 macvim reattach-to-user-namespace
+    brew update && brew upgrade -y && brew install -y tmux zsh sshrc python3 macvim reattach-to-user-namespace
   ;;
   linux*)
+    ln -s $dotfiles/linux/.zshrc ~/.zshrc.additional
+    ln -s $dotfiles/linux/.tmux.conf ~/.tmux.conf.additional
     if [ `which apt-get` ]; then
-      $prefix apt-get update && $prefix apt-get upgrade -y && $prefix apt-get install -y tmux zsh python3 python3-dev python3-pip tree vim
+       $prefix add-apt-repository ppa:russell-s-stewart/ppa && $prefix apt-get update && $prefix apt-get upgrade -y && $prefix apt-get install -y tmux zsh sshrc python3 python3-dev python3-pip tree vim xsel
     elif [ `which dnf` ]; then
-      $prefix dnf update -y && $prefix dnf install -y tmux zsh python3 python3-devel python3-pip tree vim redhat-rpm-config
+      $prefix dnf update -y && $prefix dnf install -y tmux zsh sshrc python3 python3-devel python3-pip tree vim gcc redhat-rpm-config xsel
+    elif [ `which yum` ]; then
+      $prefix yum update -y && $prefix yum install -y tmux zsh sshrc python3 python3-devel python3-pip tree vim gcc redhat-rpm-config xsel
     fi
   ;;
 esac
