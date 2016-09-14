f4c64d1543881c4fcea93b806b7b6a9800e6df3f
amaya <mail@sapphire.in.net>
2016-09-14 22:56:56 +0900

    refine init.sh

--- init.sh
+++ init.sh
@@ -1,43 +1,54 @@
 #!/bin/bash
+dotfiles=$(pwd)
+[ ! $dotfiles =~ dotfiles$ ] && echo 'execute in dotfiles directory' && exit 1
+
 git submodule init
 git submodule update
 
-dotfiles=$(pwd)
-ln -s $dotfiles/.zsh ~/.zsh
-ln -s $dotfiles/.zshrc ~/.zshrc
-ln -s $dotfiles/.tmux ~/.tmux
-ln -s $dotfiles/.tmux.conf ~/.tmux.conf
-ln -s $dotfiles/.vim ~/.vim
-ln -s $dotfiles/.vimrc ~/.vimrc
-ln -s $dotfiles/.config ~/.config
-ln -s $dotfiles/.gitconfig ~/.gitconfig
-ln -s $dotfiles/.gitignore_global ~/.gitignore_global
+[ ! -e ~/dotfiles_saved ] && mkdir ~/dotfiles_saved
+[ $# -e 1 ] && copy_mode=1
 
-if [ `which sudo` ]; then
-  prefix='sudo'
-else
-  prefix=''
-fi
+apply () {
+  from=$1
+  [ $# -eq 1 ] && to=$1 || to=$2
+  [ -e ~/$to ] && mv ~/$to ~/dotfiles_saved/$to
+  if [ $copy_mode -e 1 ]; then
+    cp -r $dotfiles/$from ~/$to
+  else
+    ln -s $dotfiles/$from ~/$to
+  fi
+}
 
 case ${OSTYPE} in
   darwin*)
-    ln -s $dotfiles/osx/.zshrc ~/.zshrc.additional
-    ln -s $dotfiles/osx/.tmux.conf ~/.tmux.conf.additional
+    apply osx/.zshrc .zshrc.additional
+    apply osx/.tmux.conf .tmux.conf.additional
     ./fonts/install.sh
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     brew update && brew upgrade -y && brew install -y tmux zsh sshrc python3 macvim reattach-to-user-namespace
   ;;
   linux*)
-    ln -s $dotfiles/linux/.zshrc ~/.zshrc.additional
-    ln -s $dotfiles/linux/.tmux.conf ~/.tmux.conf.additional
+    if [ `which sudo` ]; then
+      prefix='sudo'
+    else
+      prefix=''
+    fi
+    apply linux/.zshrc .zshrc.additional
+    apply linux/.tmux.conf .tmux.conf.additional
     if [ `which apt-get` ]; then
        $prefix add-apt-repository ppa:russell-s-stewart/ppa && $prefix apt-get update && $prefix apt-get upgrade -y && $prefix apt-get install -y tmux zsh sshrc python3 python3-dev python3-pip tree vim xsel
     elif [ `which dnf` ]; then
       $prefix dnf update -y && $prefix dnf install -y tmux zsh sshrc python3 python3-devel python3-pip tree vim gcc redhat-rpm-config xsel
     elif [ `which yum` ]; then
       $prefix yum update -y && $prefix yum install -y tmux zsh sshrc python3 python3-devel python3-pip tree vim gcc redhat-rpm-config xsel
+    else
+      echo 'unexpected distribution' && exit 1
     fi
   ;;
 esac
+
+for f in .zsh .zshrc .tmux .tmux.conf .vim .vimrc .config .gitconfig .gitignore_global; do
+  apply $f
+done
 
 pip3 install powerline-status psutil netifaces
