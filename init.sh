070dde26ebce865dae5501b7e98dfe03965502f3
amaya <git@sapphire.in.net>
2016-10-25 09:53:47 +0900

    MOD init

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ dotfiles=$(pwd)
 git submodule init
 git submodule update
 
-[ ! -e ~/dotfiles_saved ] && mkdir ~/dotfiles_saved
+[ -e ~/dotfiles_saved ] || mkdir ~/dotfiles_saved
 [ $# -eq 1 ] && copy_mode=1
 
 apply () {
@@ -29,26 +29,28 @@ case ${OSTYPE} in
   darwin*)
     apply osx/.zshrc .zshrc.additional
     apply osx/.tmux.conf .tmux.conf.additional
-    ./fonts/install.sh
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew update && brew upgrade -y && brew install -y tmux zsh sshrc python3 macvim reattach-to-user-namespace
+    brew update && brew upgrade -y && brew install -y tmux zsh python3 macvim reattach-to-user-namespace
   ;;
   linux*)
     apply linux/.zshrc .zshrc.additional
     apply linux/.tmux.conf .tmux.conf.additional
     if [ `which apt-get` ]; then
-       $prefix add-apt-repository ppa:russell-s-stewart/ppa && $prefix apt-get update && $prefix apt-get upgrade -y && $prefix apt-get install -y tmux zsh sshrc python3 python3-dev python3-pip tree vim xsel
+       $prefix add-apt-repository ppa:russell-s-stewart/ppa && $prefix apt-get update && $prefix apt-get upgrade -y && $prefix apt-get install -y tmux zsh python3 python3-dev python3-pip tree vim xsel
     elif [ `which dnf` ]; then
-      $prefix dnf update -y && $prefix dnf install -y tmux zsh sshrc python3 python3-devel python3-pip tree vim gcc redhat-rpm-config xsel
+      $prefix dnf upgrade -y && $prefix dnf install -y tmux zsh python3 python3-devel python3-pip tree vim gcc redhat-rpm-config xsel
     elif [ `which yum` ]; then
-      $prefix yum update -y && $prefix yum install -y tmux zsh sshrc python3 python3-devel python3-pip tree vim gcc redhat-rpm-config xsel
+      $prefix yum update -y && $prefix yum install -y tmux zsh python3 python3-devel python3-pip tree vim gcc redhat-rpm-config xsel
     else
       echo 'unexpected distribution' && exit 1
     fi
   ;;
 esac
 
-for f in .zsh .zshrc .tmux.conf .vim .vimrc .config .gitconfig .gitignore_global; do
+./fonts/install.sh
+
+[ -e ~/.config/ ] || mkdir ~/.config/
+for f in .zsh .zshrc .tmux.conf .vim .vimrc .gitconfig .gitignore_global .config/powerline; do
   apply $f
 done
 
